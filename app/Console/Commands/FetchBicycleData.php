9e88f15a9b3b32459fe1b3908c3e063c4aa41205
风之凌殇 <fzls.zju@gmail.com>
2016-09-01 22:05:07 +0800

    add 2 station into list

--- app/Console/Commands/FetchBicycleData.php
+++ app/Console/Commands/FetchBicycleData.php
@@ -38,72 +38,90 @@ class FetchBicycleData extends Command {
      * @return mixed
      */
     public function handle() {
-        /*run it every $T secs*/
-        $T = 30;/*seconds*/
-        while (true) {
-            /*time when loop start*/
-            $loop_start = Carbon::now();
-
-//            /*if it is during 22:00~6:00, sleep untill 6:00*/
-//            if ($loop_start->hour >= 22 || $loop_start->hour <= 6) {
-//                /*get the today's 6:00*/
-//                $next_morning = Carbon::now()->startOfDay()->addHours(6);
-//                /*if it is later than 22:00, add a day*/
-//                if ($loop_start->hour >= 22) {
-//                    $next_morning = $next_morning->addDay();
-//                }
-//                $time_to_sleep = $next_morning->diffInSeconds($loop_start);
-//                \Log::info('Sleep during 22:00~6:00');
-//                sleep($time_to_sleep);
-//            }
-            \Log::info('Start a new loop');
-
-            $client = new Client();
-
-            $start = Carbon::now();
-            /*Android*/
-            foreach (\Config::get('data.id_android') as $name => $id) {
-                try {
-                    $res  = $client->get("http://bike.hz.dingdatech.com/service/bicycle/stations/$id");
-                    $json = \GuzzleHttp\json_decode($res->getBody(), true);
-                    if ($json['meta']['code'] === 200) {
-                        BicycleDatum::create($json['data']['station']);
-                        \Log::info(sprintf('Insert record for station: %s', $name));
-                    } else {
-                        \Log::notice(sprintf("Error when insert record for station %s failed with code:%d, message:%s", $name, $json['meta']['code'], $json['meta']['message']));
-                    }
-                } catch (\Exception $e) {
-                    \Log::error(sprintf("Failed to fetch station $name with exception %s", $e->getMessage()));
+        /*TODO : delete it later*/
+        $client = new Client();
+        for ($i = 1; $i < 4000; ++$i) {
+            $id = sprintf('%05d',$i);
+            try {
+                $res  = $client->get("http://bike.hz.dingdatech.com/service/bicycle/stations/20160802161027hzl$id");
+                $json = \GuzzleHttp\json_decode($res->getBody(), true);
+                if ($json['meta']['code'] === 200) {
+                    BicycleDatum::create($json['data']['station']);
+                    \Log::info(sprintf('Insert record for station: %s', $json['data']['station']['name']));
+                } else {
+                    \Log::notice(sprintf("Error when insert record for station %s failed with code:%d, message:%s", $json['data']['station']['name'], $json['meta']['code'], $json['meta']['message']));
                 }
+            } catch (\Exception $e) {
+                \Log::error(sprintf("Failed to fetch station $id with exception %s", $e->getMessage()));
             }
-            \Log::info(sprintf("Get from Android api with %d seconds", Carbon::now()->diffInSeconds($start)));
-            /*Wechat*/
-            /*TODO: 这个接口目前由于微信端系统升级，暂不可用*/
-//        $start = Carbon::now();
-//        try {
-//            $res  = $client->get('http://c.ggzxc.com.cn/wz/np_getBikesByWeiXin.do?lng=120.107&lat=30.295301&len=800&_=1472267874845');
-//            $json = \GuzzleHttp\json_decode($res->getBody(), true);
-//            if ($res->getStatusCode() === 200) {
-//                foreach ($json['data'] as $record) {
-//                    BicycleDataWechat::create($record);
-//                    \Log::info(sprintf('insert record for station: %s<br>', $record['name']));
-//                }
-//            } else {
-//                \Log::notice(sprintf("error when insert record for station %s failed with code:%d, message:%s", $json['data']['station']['name'], $json['meta']['code'], $json['meta']['message']));
-//            }
-//        } catch (\Exception $e) {
-//            \Log::error(sprintf("failed to fetch station $id with exception %s"), $e->getMessage());
-//        }
-//        \Log::info(sprintf("Get from Wechat api with %s",Carbon::now()->diffForHumans($start)));
-
-            /*time when loop end*/
-            $loop_end = Carbon::now();
-            /*calculate time to sleep*/
-            $duration      = $loop_end->diffInSeconds($loop_start);
-            $time_to_sleep = $T - $duration;
-            /*sleep for some tiem to make it T per loop*/
-            \Log::info(sprintf("Sleep for %d seconds", $time_to_sleep));
-            sleep($time_to_sleep);
         }
+
+//        /*run it every $T secs*/
+//        $T = 30;/*seconds*/
+//        while (true) {
+//            /*time when loop start*/
+//            $loop_start = Carbon::now();
+//
+////            /*if it is during 22:00~6:00, sleep untill 6:00*/
+////            if ($loop_start->hour >= 22 || $loop_start->hour <= 6) {
+////                /*get the today's 6:00*/
+////                $next_morning = Carbon::now()->startOfDay()->addHours(6);
+////                /*if it is later than 22:00, add a day*/
+////                if ($loop_start->hour >= 22) {
+////                    $next_morning = $next_morning->addDay();
+////                }
+////                $time_to_sleep = $next_morning->diffInSeconds($loop_start);
+////                \Log::info('Sleep during 22:00~6:00');
+////                sleep($time_to_sleep);
+////            }
+//            \Log::info('Start a new loop');
+//
+//            $client = new Client();
+//
+//            $start = Carbon::now();
+//            /*Android*/
+//            foreach (\Config::get('data.id_android') as $name => $id) {
+//                try {
+//                    $res  = $client->get("http://bike.hz.dingdatech.com/service/bicycle/stations/$id");
+//                    $json = \GuzzleHttp\json_decode($res->getBody(), true);
+//                    if ($json['meta']['code'] === 200) {
+//                        BicycleDatum::create($json['data']['station']);
+//                        \Log::info(sprintf('Insert record for station: %s', $name));
+//                    } else {
+//                        \Log::notice(sprintf("Error when insert record for station %s failed with code:%d, message:%s", $name, $json['meta']['code'], $json['meta']['message']));
+//                    }
+//                } catch (\Exception $e) {
+//                    \Log::error(sprintf("Failed to fetch station $name with exception %s", $e->getMessage()));
+//                }
+//            }
+//            \Log::info(sprintf("Get from Android api with %d seconds", Carbon::now()->diffInSeconds($start)));
+//            /*Wechat*/
+//            /*TODO: 这个接口目前由于微信端系统升级，暂不可用*/
+////        $start = Carbon::now();
+////        try {
+////            $res  = $client->get('http://c.ggzxc.com.cn/wz/np_getBikesByWeiXin.do?lng=120.107&lat=30.295301&len=800&_=1472267874845');
+////            $json = \GuzzleHttp\json_decode($res->getBody(), true);
+////            if ($res->getStatusCode() === 200) {
+////                foreach ($json['data'] as $record) {
+////                    BicycleDataWechat::create($record);
+////                    \Log::info(sprintf('insert record for station: %s<br>', $record['name']));
+////                }
+////            } else {
+////                \Log::notice(sprintf("error when insert record for station %s failed with code:%d, message:%s", $json['data']['station']['name'], $json['meta']['code'], $json['meta']['message']));
+////            }
+////        } catch (\Exception $e) {
+////            \Log::error(sprintf("failed to fetch station $id with exception %s"), $e->getMessage());
+////        }
+////        \Log::info(sprintf("Get from Wechat api with %s",Carbon::now()->diffForHumans($start)));
+//
+//            /*time when loop end*/
+//            $loop_end = Carbon::now();
+//            /*calculate time to sleep*/
+//            $duration      = $loop_end->diffInSeconds($loop_start);
+//            $time_to_sleep = $T - $duration;
+//            /*sleep for some tiem to make it T per loop*/
+//            \Log::info(sprintf("Sleep for %d seconds", $time_to_sleep));
+//            sleep($time_to_sleep);
+//        }
     }
 }
