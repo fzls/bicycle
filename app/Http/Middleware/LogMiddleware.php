3ac586a1bc30371bc13b525c58db40623a0e68eb
风之凌殇 <fzls.zju@gmail.com>
2016-09-04 00:28:33 +0800

    add log middleware to log user ip

---
+++ app/Http/Middleware/LogMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Log;
+
+class LogMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        \Log::notice(sprintf('ip : %s visited %s',$request->ip(),$request->url()));
+
+        return $next($request);
+    }
+}
