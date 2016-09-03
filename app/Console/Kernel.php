62a046f58f4a8e7a39637d6a08955364385a3bd7
风之凌殇 <fzls.zju@gmail.com>
2016-09-04 03:59:47 +0800

    test new deployment script

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('fetch:bicycle_data')
-            ->everyMinute();
+            ->everyMinute()->withoutOverlapping();
     }
 
     /**
